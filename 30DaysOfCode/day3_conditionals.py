d624d6373b147f98b72777be2eb53af3aa97c2e1
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-06-29 11:07:51 -0400

    completed day 3 of 30 days of code

---
+++ 30DaysOfCode/day3_conditionals.py
@@ -0,0 +1,16 @@
+""" If is odd, print Weird
+If is even and in the inclusive range of 2 to 5, print 'Not Weird'
+If is even and in the inclusive range of 6 to 20, print 'Weird'
+If is even and greater than 20, print 'Not Weird' """
+
+# N = 3
+N = 24
+
+if N % 2 != 0:
+    print('Weird')
+elif N % 2 == 0 and N in range(2, 6):
+        print("Not Weird")
+elif N % 2 == 0 and N in range(6, 21):
+        print("Weird")
+elif N % 2 == 0 and N > 20:
+        print("Not Weird")
