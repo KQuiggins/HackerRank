6e7599383e4f929b42169e16ba3402038539ae70
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-07-04 07:57:56 -0400

    completed day 7 of 30 days of code

---
+++ 30DaysOfCode/day7_arrays.py
@@ -0,0 +1,8 @@
+""" Task
+Given an array, A , of N integers, print A's elements in reverse order as a single line of space-separated numbers.  """
+
+arr = [1, 4, 3, 2]
+
+for i in arr[::-1]:
+    print(i, '', end='')
+
