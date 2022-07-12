bb1fcfcf812f8aa5cdd408171251984287899a03
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-07-12 11:48:57 -0400

    completed day 11 2D Arrays

--- day11_2DArrays.py
+++ day11_2DArrays.py
@@ -18,6 +18,27 @@ print(hourGlass_1)
 
 hourGlass_2 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i +2][ j + 2]
 
-print(hourGlass_2)
+print(hourGlass_2) 
 
-for i 
+def get_hourglass_sum(matrix, row, col):
+      sum = 0
+      sum += matrix[row - 1][col - 1] # get value top-left
+      sum += matrix[row - 1][col]     # get value directly above
+      sum += matrix[row - 1][col + 1] # get value top-right
+      sum += matrix[row][col]         # element itself
+      sum += matrix[row + 1][col -1]  # get value below to left
+      sum += matrix[row + 1][col]     # get value directly below
+      sum += matrix[row + 1][col + 1] # get value below to the right
+
+      return sum
+
+max_hourglass_sum = -63
+
+for i in range(1,5):
+      for j in range(1,5):
+            current_hourglass_sum = get_hourglass_sum(arr, i, j)
+            if current_hourglass_sum > max_hourglass_sum:
+                  max_hourglass_sum = current_hourglass_sum
+
+print(max_hourglass_sum)
+
